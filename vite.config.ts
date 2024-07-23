8fe3b01bd56a2a02ec4e6109b6f9cf997bb806c5
Atte Virtanen <aivirtan@gmail.com>
2024-07-23 21:05:05 +0300

    configure manifest and webpage meta

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,25 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(),
-  VitePWA({ registerType: 'autoUpdate' }),
+  VitePWA({ registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+    manifest: {
+      name: 'Noppa — A Virtual Dice',
+      short_name: 'Noppa',
+      description: 'A dice for any options: An app (PWA) for randomly picking an option from a a list which the user can enter.',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: 'icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }}),
   ],
 })
