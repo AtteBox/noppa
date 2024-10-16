cabe94ff9b40ee188afd1d188068294349037cca
Atte Virtanen <90787140+AtteBox@users.noreply.github.com>
2024-10-17 02:41:56 +0300

    Chore: Add Linting And Formatting (#6)
    * initial refactoring
    
    * install prettier
    
    * ran formatting
    
    * fixed typo
    
    * install eslint and fix color generation function
    
    * remove function etc. comma dingle
    
    * still fix comma dangle
    
    * manual fixes
    
    * comma dingle pt. 3

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["plugin:svelte/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+  ignorePatterns: ["svelte.config.js"],
+  rules: {
+    "comma-dangle": ["error", {
+      "arrays": "never",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "never"
+    }]
+  },
+};
