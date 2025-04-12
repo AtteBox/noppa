46a5cc3115afc1687024a3ba5792f9fbfab209ed
Atte Virtanen <90787140+AtteBox@users.noreply.github.com>
2025-04-12 23:03:45 +0300

    improve animation and add link to Github repo (#42)

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import githubLogo from "./assets/github-mark-white.svg";
   import VirtualDice from "./lib/components/VirtualDice.svelte";
 </script>
 
@@ -14,6 +15,13 @@
 <footer>
   <p>
     © Atte Virtanen {new Date().getFullYear()}
+    <a
+      href="https://github.com/AtteBox/noppa"
+      title="Open code in Github"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><img id="github-link-logo" src={githubLogo} alt="Github logo" /></a
+    >
   </p>
 </footer>
 
@@ -21,5 +29,20 @@
   .read-the-docs {
     color: #888;
     margin-bottom: 2rem;
+  }
+
+  #github-link-logo {
+    margin-top: -0.2rem;
+    height: 1.5rem;
+    margin-left: 0.8rem;
+    vertical-align: middle;
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+    padding: 1rem;
   }
 </style>
